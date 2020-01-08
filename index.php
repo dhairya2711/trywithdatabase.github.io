064d6542c7571c47fc8e2c1b834fc95d0ee8f973
dhairya2711 <58970164+dhairya2711@users.noreply.github.com>
2020-01-08 13:08:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try{
-  $pdo = new PDO("mysql:host=localhost; dbname=000797437", "000797437", "20001128");
+  $pdo = new PDO("mysql:host=localhost:1080; dbname=000797437", "000797437", "20001128");
   }catch(Exception $e){
     echo "There is a problem to connect with database";
   }
